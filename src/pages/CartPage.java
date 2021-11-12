5c9885e3421b005cd9987dcc2dafb3c70c78eba6
Utkal Barik <utkal.barik@gmail.com>
2021-11-12 08:54:39 +0530

    some more updates

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -2,4 +2,7 @@ package pages;
 
 public class CartPage {
 	String product="TV"; 
+/*	public static void main(String[] args) {
+		System.out.println("cart page");
+	}*/
 }
--- src/test/CartTest.java
+++ src/test/CartTest.java
@@ -1,5 +1,5 @@
 package test;
 
 public class CartTest {
-	
-}
+/*	String str="test";
+*/}
