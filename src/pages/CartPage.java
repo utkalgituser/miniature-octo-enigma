1e31e97dceaac46ed60cf7b295d8b20ce5abffc7
Utkal Barik <utkal.barik@gmail.com>
2021-11-12 09:01:39 +0530

    deleted cart files

--- src/pages/CartPage.java
+++
@@ -1,8 +0,0 @@
-package pages;
-
-public class CartPage {
-	String product="TV"; 
-/*	public static void main(String[] args) {
-		System.out.println("cart page");
-	}*/
-}
--- src/test/CartTest.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public class CartTest {
-/*	String str="test";
-*/}
