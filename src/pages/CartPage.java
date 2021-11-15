c60230c7936b743ef6378819002b3efda1306010
utkal keshari barik <49710146+utkalgituser@users.noreply.github.com>
2021-11-15 11:39:13 +0530

    Create CartPage.java

---
+++ src/pages/CartPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class CartPage {
+	public static void main(String[] argd) {
+		System.out.println("this is cart page");
+	}
+	public void cart() {
+    String name="CartD2";
+		System.out.println("this is my cart page"+name);
+	}
+}
