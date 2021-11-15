5a5b55f29dde05fb39688301bb3ad88301c24a2c
utkal keshari barik <49710146+utkalgituser@users.noreply.github.com>
2021-11-15 11:45:22 +0530

    Update CartPage.java

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -1,11 +1,16 @@
 package pages;
 
 public class CartPage {
+	public String name="";
 	public static void main(String[] argd) {
 		System.out.println("this is cart page");
 	}
 	public void cart() {
     String name="CartD1";
+		System.out.println("this is my cart page"+name);
+	}
+	public void cart(String name) {
+
 		System.out.println("this is my cart page"+name);
 	}
 }
